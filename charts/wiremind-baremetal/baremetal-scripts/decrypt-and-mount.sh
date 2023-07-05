c1011f4b6540a86c856ee5e1e6b781c7a0ee6842
Maxime Leroy <19607336+maxime1907@users.noreply.github.com>
2023-07-05 16:04:45 +0200

    fix(wiremind-baremetal): raid check return code (#291)

--- charts/wiremind-baremetal/baremetal-scripts/decrypt-and-mount.sh
+++ charts/wiremind-baremetal/baremetal-scripts/decrypt-and-mount.sh
@@ -2,18 +2,21 @@
 set -x
 set -e
 
-RAID_DEVICE="/dev/md0"
-RAID_DEVICE_UNLOCKED="/dev/md/md0"
+RAID_DEVICE="/dev/md/md0"
 DECRYPTED_LUKS_DEVICE_NAME="md0crypt"
 DECRYPTED_LUKS_DEVICE="/dev/mapper/md0crypt"
 LVM_PARTITION_DEVICES="/dev/md0cryptlvm/persistentvolume*"
 
+# Check if raid device exists
+set +e
+
 device_details=$(mdadm -D "$RAID_DEVICE")
-if [ $? != 0 ]; then
+if [[ "$?" != "0" ]]; then
     echo "Raid device <${RAID_DEVICE}> not found, skipping"
     exit 0
 fi
 
+set -e
 
 wmb_decrypt_device() {
   if ! cryptsetup status $DECRYPTED_LUKS_DEVICE_NAME; then
@@ -22,7 +25,7 @@ wmb_decrypt_device() {
       # XXX: this will actually hang (even if successfully opening) when run like this.
       # When run locally, even from a script, it works.
       # It will anyway get killed by the livenessprobe.
-      echo -n "$password" | cryptsetup luksOpen --verbose $RAID_DEVICE_UNLOCKED $DECRYPTED_LUKS_DEVICE_NAME --verbose --debug --allow-discards --key-file -
+      echo -n "$password" | cryptsetup luksOpen --verbose $RAID_DEVICE $DECRYPTED_LUKS_DEVICE_NAME --verbose --debug --allow-discards --key-file -
       echo "Done decrypting the device $DECRYPTED_LUKS_DEVICE_NAME."
       set -x
   fi
