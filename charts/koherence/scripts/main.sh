25d66e057aa6f77761a44b88ddb2a05567f35af2
Anthony Delannoy <adelannoy@wiremind.io>
2023-10-17 19:46:04 +0200

    fix(koherence): use correct command & no probes for now

--- charts/koherence/scripts/main.sh
+++ charts/koherence/scripts/main.sh
@@ -2,8 +2,8 @@
 
 set -xe
 
-# Copy scripts to host
-/bin/cp /tmp/script/* /usr/bin/koherence /usr/bin/jq -t /run/koherence/
+# Copy binaries to host
+/bin/cp /usr/bin/koherence /usr/bin/jq -t /run/koherence/
 
 while true; do
     /bin/nsenter -m/proc/1/ns/mnt /run/koherence/koherence check bs | /run/koherence/jq '.diff'
