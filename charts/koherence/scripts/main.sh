bfbcc29bc05f325061d8b29f5548aab38095c9c9
Anthony Delannoy <adelannoy@wiremind.io>
2023-10-18 11:22:24 +0200

    chore(koherence): dump diff in file instead of stdout

--- charts/koherence/scripts/main.sh
+++ charts/koherence/scripts/main.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-set -xe
+set -x
+set -e
 
 # Copy binaries to host
 /bin/cp /usr/bin/koherence /usr/bin/jq -t /run/koherence/
 
 while true; do
-    /bin/nsenter -m/proc/1/ns/mnt /run/koherence/koherence check bs | /run/koherence/jq '.diff'
+    # Do not redirect even to jq to exit in case of error
+    /bin/nsenter -m/proc/1/ns/mnt /run/koherence/koherence check bs 1> /run/koherence/diff.json
     sleep $KOHERENCE_SLEEP_SECONDS
 done
