7b915acf2cdcce5af21ef824a0f041cf669b19d8
Antoine Clarman <aclarman@wiremind.io>
2024-05-07 14:36:50 +0200

    feat(koherence): add metrics

--- charts/koherence/scripts/main.sh
+++ charts/koherence/scripts/main.sh
@@ -6,8 +6,4 @@ set -e
 # Copy binaries to host
 /bin/cp /usr/bin/koherence /usr/bin/jq -t /run/koherence/
 
-while true; do
-    # Do not redirect even to jq to exit in case of error
-    /bin/nsenter -m/proc/1/ns/mnt /run/koherence/koherence check bs 1> /run/koherence/diff.json
-    sleep $KOHERENCE_SLEEP_SECONDS
-done
+/bin/nsenter -m/proc/1/ns/mnt /run/koherence/koherence serve
